34cc5404f1cad03fbd374d555a7097b36aae4592
Kian_School <24-0203c@CCL1-10>
2025-06-10 12:48:30 +0800

    Add project files.

---
+++ ATM_Machine_DSA_Prog2_Finals/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ATM_Machine_DSA_Prog2_Finals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string data_directory_path = "Account_Data";
+            string accounts_path = $"{data_directory_path}/accounts.csv";
+            string reciepts_path = "Reciepts_Log";
+
+            if (!Directory.Exists(data_directory_path))
+                Directory.CreateDirectory(data_directory_path);
+            if (!Directory.Exists(reciepts_path))
+                Directory.CreateDirectory(reciepts_path);
+            if (!File.Exists(accounts_path))
+                File.Create(accounts_path);
+        }
+    }
+}
