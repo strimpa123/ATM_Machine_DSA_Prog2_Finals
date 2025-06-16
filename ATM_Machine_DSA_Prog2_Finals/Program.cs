30387a6942e1918afb097c486c7ef80f50ba4ec3
Kian_School <24-0203c@CCL1-10>
2025-06-16 10:28:11 +0800

    added authentication

--- ATM_Machine_DSA_Prog2_Finals/Program.cs
+++ ATM_Machine_DSA_Prog2_Finals/Program.cs
@@ -23,29 +23,49 @@ namespace ATM_Machine_DSA_Prog2_Finals
                 File.AppendAllText(accounts_path, "Name,Account Number,PIN,Balance");
                 
 
-            Console.WriteLine("Enter Your Account Number: ");
-            string user_account_entry = Console.ReadLine();
+            Console.WriteLine("Insert Your Card (Path): ");
+            string user_card_path = Console.ReadLine();
+
             bool admin = false;
             string[] accounts = File.ReadAllLines(accounts_path);
-            string[] account_info; // Format = [0]Name , [1]Account Number , [2]PIN , [3]Balance
+            string[] account_info;
 
-            if (user_account_entry == "5426")
+            if (user_card_path == "5426")
                 admin = true;
             else
             {
                 admin = false;
-                for (int i = 1; i < accounts.Length; i++)
+                if (!File.Exists(user_card_path))
+                    Console.WriteLine("Card Not Found!");
+                else
                 {
-                    account_info = accounts[i].Split(',');
-                    if (user_account_entry == account_info[1]) 
-                        break;
-                }
+                    string[] user_card = File.ReadAllLines(user_card_path);
+                    string[] user_card_info = user_card[1].Split(','); // [0]Account Number, [1]Firstname , [2]Lastname , [3]CVV ---(CARD FORMAT)
+                    for (int i = 1; i < accounts.Length; i++)
+                    {
+                        account_info = accounts[i].Split(','); // [0]Name , [1]Account Number , [2]PIN , [3]Balance ---(ACCOUNT FORMAT)
+                        if (user_card_info[0] == account_info[1])
+                        {
+                            Console.WriteLine("Enter PIN: ");
+                            string user_card_pin = Console.ReadLine();
+                            if (user_card_pin == account_info[2])
+                                break;
+                            else
+                                Console.WriteLine("Wrong PIN! || Ejecting Card...");
+                        }
+                        if (i + 1 == accounts.Length)
+                            Console.WriteLine("Invalid Bank Account Card! || Ejecting Card...");
+                    }
+                }    
+                Console.ReadKey();
             }
                 
 
             if (admin) // Admin Access
             {
-                Console.WriteLine("is admin");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Clear();
+                Console.WriteLine("------([Administrator Menu])------");
             }
             if (!admin) // User Access
             {
