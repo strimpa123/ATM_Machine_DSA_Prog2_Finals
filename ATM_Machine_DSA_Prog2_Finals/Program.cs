ba200771d40fe1b45599e56da2b948cf95807e28
Kian_School <24-0203c@CCL1-10>
2025-06-18 10:26:04 +0800

    added new functions

--- ATM_Machine_DSA_Prog2_Finals/Program.cs
+++ ATM_Machine_DSA_Prog2_Finals/Program.cs
@@ -12,7 +12,8 @@ namespace ATM_Machine_DSA_Prog2_Finals
         static void Main(string[] args)
         {
             string data_directory_path = "Account_Data";
-            string accounts_path = $"{data_directory_path}/accounts.csv"; 
+            string accounts_path = $"{data_directory_path}/accounts.csv";
+            string admin_acc_path = $"{data_directory_path}/admin_accounts.csv";
             string reciepts_path = "Reciepts_Log";
 
             if (!Directory.Exists(data_directory_path))
@@ -21,53 +22,75 @@ namespace ATM_Machine_DSA_Prog2_Finals
                 Directory.CreateDirectory(reciepts_path);
             if (!File.Exists(accounts_path))
                 File.AppendAllText(accounts_path, "Name,Account Number,PIN,Balance");
-                
+            if (!File.Exists(admin_acc_path))
+                File.AppendAllText(admin_acc_path, "kian,sarah");
 
-            Console.WriteLine("Insert Your Card (Path): ");
-            string user_card_path = Console.ReadLine();
-
+            bool authentication = true;
             bool admin = false;
-            string[] accounts = File.ReadAllLines(accounts_path);
-            string[] account_info;
-
-            if (user_card_path == "5426")
-                admin = true;
-            else
+            while (authentication)
             {
-                admin = false;
-                if (!File.Exists(user_card_path))
-                    Console.WriteLine("Card Not Found!");
+                Console.WriteLine("Insert Your Card (Path): ");
+                string user_card_path = Console.ReadLine();
+
+                
+                string[] accounts = File.ReadAllLines(accounts_path);
+                string[] account_info;
+
+                if (user_card_path == "5426")
+                {
+                    string[] admins_account = File.ReadAllLines(admin_acc_path);
+                    Console.WriteLine("Enter Account Name");
+                    string admin_name = Console.ReadLine();
+                    for (int i = 0; i < admins_account.Length; i++)
+                    {
+                        string[] admin_info = admins_account[i].Split(','); // [0]Account Names, [1] Passwords
+                        if (admin_name == admin_info[0])
+                        {
+                            Console.WriteLine("Enter Password: ");
+                            string admin_pass = Console.ReadLine();
+                            if (admin_pass == admin_info[1])
+                                admin = true;
+                            else
+                                Console.WriteLine("Wrong Password");
+                        }
+                        if (i + 1 >= admins_account.Length)
+                            Console.WriteLine("Account Does not Exist");
+                    }
+                }
                 else
                 {
-                    string[] user_card = File.ReadAllLines(user_card_path);
-                    string[] user_card_info = user_card[1].Split(','); // [0]Account Number, [1]Firstname , [2]Lastname , [3]CVV ---(CARD FORMAT)
-                    for (int i = 1; i < accounts.Length; i++)
+                    admin = false;
+                    if (!File.Exists(user_card_path))
+                        Console.WriteLine("Card Not Found!");
+                    else
                     {
-                        account_info = accounts[i].Split(','); // [0]Name , [1]Account Number , [2]PIN , [3]Balance ---(ACCOUNT FORMAT)
-                        if (user_card_info[0] == account_info[1])
+                        string[] user_card = File.ReadAllLines(user_card_path);
+                        string[] user_card_info = user_card[1].Split(','); // [0]Account Number, [1]Firstname , [2]Lastname , [3]CVV ---(CARD FORMAT)
+                        for (int i = 0; i < accounts.Length; i++)
                         {
-                            Console.WriteLine("Enter PIN: ");
-                            string user_card_pin = Console.ReadLine();
-                            if (user_card_pin == account_info[2])
-                                break;
-                            else
-                                Console.WriteLine("Wrong PIN! || Ejecting Card...");
+                            account_info = accounts[i].Split(','); // [0]Name , [1]Account Number , [2]PIN , [3]Balance ---(ACCOUNT FORMAT)
+                            if (user_card_info[0] == account_info[1])
+                            {
+                                Console.WriteLine("Enter Account PIN: ");
+                                string user_card_pin = Console.ReadLine();
+                                if (user_card_pin == account_info[2])
+                                    break;
+                                else
+                                    Console.WriteLine("Wrong PIN! || Ejecting Card...");
+                            }
+                            if (i + 1 >= accounts.Length)
+                                Console.WriteLine("Invalid Bank Account Card! || Ejecting Card...");
                         }
-                        if (i + 1 == accounts.Length)
-                            Console.WriteLine("Invalid Bank Account Card! || Ejecting Card...");
                     }
-                }    
-                Console.ReadKey();
+                }
             }
-                
-
-            if (admin) // Admin Access
+            if (admin) // Admin Menu
             {
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.Clear();
                 Console.WriteLine("------([Administrator Menu])------");
             }
-            if (!admin) // User Access
+            if (!admin) // User Menu
             {
                 Console.WriteLine("is user");
             }
